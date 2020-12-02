ec7d1cc7df2b6e489687f521f0e46ec4f967379b
Sylvie-B <unsolo@hotmail.fr>
2020-12-02 10:39:23 +0100

    fonction texte

---
+++ app.js
@@ -0,0 +1,9 @@
+function myUpdate (idPar, txt){
+    document.getElementById(idPar).innerHTML = txt;
+}
+
+myUpdate("p1", "j'écris");
+myUpdate("p2", "un texte");
+myUpdate("p3", "différent");
+myUpdate("p4", "dans chaque");
+myUpdate("p5", "paragraphe");
